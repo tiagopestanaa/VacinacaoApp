7376bd561323b49038abbb84500a2b8e55908837
tiagopestanaa <tiago.pestana@nexllence.com>
2021-10-11 13:55:03 +0100

    version 2

--- vacinacaoapp.js
+++ vacinacaoapp.js
@@ -1,4 +1,4 @@
-const nome = "VacinacaoCovid19"; // Declarar uma constante e atribuir-lhe o valor "VacinacaoCovid19" (um 'string', uma cadeia de carateres)
+const nome = "VacinacaoCovid19"; // Declarar uma constante e atribuir-lhe o valor "VacinacaoCovid-19" (um 'string', uma cadeia de carateres)
 
  // Definir tipos (classes) de objetos que a app gerirá ("type of objects")
 class Utente {}
