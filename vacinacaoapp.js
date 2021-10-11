677ab9e8233dd0d063493b4f222087612839c4f2
tiagopestanaa <tiago.pestana@nexllence.com>
2021-10-11 16:53:55 +0100

    version 4

--- vacinacaoapp.js
+++ vacinacaoapp.js
@@ -2,37 +2,71 @@ const nome = "VacinacaoCovid19"; // Declarar uma constante e atribuir-lhe o valo
 
  // Definir tipos (classes) de objetos que a app gerirá ("type of objects")
 class Utente {
-    Nome;
-    NrUtente;
-    Genero;
-    #Nascimento;
+    nome;
+    numero;
+    genero;
+    #nascimento;
 
-    constructor(Nome, NrUtente, Genero) { // Ao adicionar construtores, já não é necessário definir campos em instruções separadas
-        this.Nome = Nome; // Declaração de campos que identificarão os utentes
-        this.NrUtente = NrUtente; 
-        this.Genero = Genero;
-    /*toString() { 
-        return `${this.InformacaoUtente} (° ${this.#Nascimento})`;*/
+    constructor(nome, numero, genero) { // Ao adicionar construtores, já não é necessário definir campos em instruções separadas
+        this.nome = nome; // Declaração de campos que identificarão os utentes
+        this.numero = numero; 
+        this.genero = genero;
+    }
+    toString() { 
+        return `${this.InformacaoUtente} (° ${this.#nascimento})`;
     }
     get InformacaoUtente () { // Propriedade (método "getter") que reproduz a informação completa do Utente
-        return `${this.Nome} ${this.NrUtente} ${this.Genero}`; // String template syntax (alternativa a "adição" de strings com o operador '+')
+        return `${this.nome} ${this.nrUtente} ${this.genero}`; // String template syntax (alternativa a "adição" de strings com o operador '+')
     }
     set Nascimento(data) { // Propriedade (método "setter") que coloca um valor no campo privado (o que não foi feito no construtor)
-        this.#Nascimento = data; // Este campo é privado, isto é, invisível para outros objetos
+        this.#nascimento = data; // Este campo é privado, isto é, invisível para outros objetos
     }
 }
 
 class Vacina {
-    Nome;
-    EDoseUnica;
-    IntervaloEntreDoses;
+    nome;
+    tipo;
+    eficacia;
+    nrAdministracoes; 
+    intervaloEntreDoses; /*Número de semanas*/
 
+    constructor(nome,tipo,eficacia, nrAdministracoes, intervaloEntreDoses) { // Ao adicionar construtores, já não é necessário definir campos em instruções separadas
+        this.nome = nome; // Declaração de campos que identificarão as vacinas
+        this.tipo = tipo; 
+        this.eficacia = eficacia;
+        this.nrAdministracoes = nrAdministracoes;
+        this.intervaloEntreDoses = intervaloEntreDoses;
+    }
+    
+    toString() { 
+        return `${this.InformacaoVacina}`;
+    }
+    get InformacaoVacina () { // Propriedade (método "getter") que reproduz a informação completa da Vacina
+        return `${this.nome} ${this.tipo} ${this.eficacia} ${this.nrAdministracoes} ${this.intervaloEntreDoses}`; // String template syntax (alternativa a "adição" de strings com o operador '+')
+    }
+    
 }
 
-class Centro_Vacinacao {
-    Nome;
-    Morada;
-    Distrito;
+class CentroVacinacao {
+    nome;
+    morada;
+    distrito;
+
+    constructor(nome, morada, distrito) { // Ao adicionar construtores, já não é necessário definir campos em instruções separadas
+        this.nome = nome; // Declaração de campos que identificarão os centros de vacinação
+        this.morada = morada; 
+        this.distrito = distrito;
+    }
+
+    toString() { 
+        return `${this.InformacaoCentroVacinacao}`;
+    }
+    get InformacaoCentroVacinacao () { // Propriedade (método "getter") que reproduz a informação completa do Utente
+        return `${this.Nome} ${this.Morada} ${this.Distrito}`; // String template syntax (alternativa a "adição" de strings com o operador '+')
+    }
+    set Nascimento(data) { // Propriedade (método "setter") que coloca um valor no campo privado (o que não foi feito no construtor)
+        this.#Nascimento = data; // Este campo é privado, isto é, invisível para outros objetos
+    }
 }
 
 class Vacinacao {
@@ -40,6 +74,6 @@ class Vacinacao {
     Vacina;
     Centro_Vacinacao;
     Data;
-    NrDose;
+    NrDose;/*1ª,2ª,3ª...*/
 
 }
