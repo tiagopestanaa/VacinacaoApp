bbbfdc4f294bc4e616314202e02aec82c86241d8
tiagopestanaa <tiago.pestana@nexllence.com>
2021-10-11 15:56:37 +0100

    version 4

--- vacinacaoapp.js
+++ vacinacaoapp.js
@@ -1,10 +1,45 @@
 const nome = "VacinacaoCovid19"; // Declarar uma constante e atribuir-lhe o valor "VacinacaoCovid-19" (um 'string', uma cadeia de carateres)
 
  // Definir tipos (classes) de objetos que a app gerirá ("type of objects")
-class Utente {}
+class Utente {
+    Nome;
+    NrUtente;
+    Genero;
+    #Nascimento;
 
-class Vacina {}
+    constructor(Nome, NrUtente, Genero) { // Ao adicionar construtores, já não é necessário definir campos em instruções separadas
+        this.Nome = Nome; // Declaração de campos que identificarão os utentes
+        this.NrUtente = NrUtente; 
+        this.Genero = Genero;
+    toString() { 
+        return `${this.InformacaoUtente} (° ${this.#Nascimento})`;
+    }
+    get InformacaoUtente () { // Propriedade (método "getter") que reproduz a informação completa do Utente
+        return `${this.Nome} ${this.NrUtente} ${this.Genero}`; // String template syntax (alternativa a "adição" de strings com o operador '+')
+    }
+    set Nascimento(data) { // Propriedade (método "setter") que coloca um valor no campo privado (o que não foi feito no construtor)
+        this.#Nascimento = data; // Este campo é privado, isto é, invisível para outros objetos
+    }
+}
 
-class Centro_Vacinacao {}
+class Vacina {
+    Nome;
+    EDoseUnica;
+    IntervaloEntreDoses;
 
-class Vacinacao {}
+}
+
+class Centro_Vacinacao {
+    Nome;
+    Morada;
+    Distrito;
+}
+
+class Vacinacao {
+    Utente;
+    Vacina;
+    Centro_Vacinacao;
+    Data;
+    NrDose;
+
+}
